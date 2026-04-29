6a00998f86856263b8ef82e1146c366ed7f60ab1
bytepassperks <learn@skillmatch.co.site>
2026-04-29 00:47:56 +0000

    break test: expect wrong value to trigger CI failure

--- test.js
+++ test.js
@@ -5,8 +5,8 @@ function add(a, b) {
   return a + b;
 }
 
-// Tests
-assert.strictEqual(add(1, 2), 3, "1 + 2 should equal 3");
+// Tests - BROKEN: expects 4 instead of 3
+assert.strictEqual(add(1, 2), 4, "1 + 2 should equal 4");
 assert.strictEqual(add(-1, 1), 0, "-1 + 1 should equal 0");
 assert.strictEqual(add(0, 0), 0, "0 + 0 should equal 0");
 
