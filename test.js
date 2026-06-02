4ca307c9a9875798de631ab66ef86e92bcde0ad6
bytepassperks <support@warpfix.org>
2026-06-02 16:04:53 +0000

    Move calculator logic to src/ and add tests
    add() has a bug (returns a-b); tests in test.js fail on it.

--- test.js
+++ test.js
@@ -1,13 +1,10 @@
 // Simple test file
 const assert = require("assert");
+const { add, subtract, multiply } = require("./src/calculator");
 
-function add(a, b) {
-  return a + b;
-}
-
-// Tests - BROKEN: expects 4 instead of 3
-assert.strictEqual(add(1, 2), 4, "1 + 2 should equal 4");
+assert.strictEqual(add(2, 3), 5, "2 + 3 should equal 5");
 assert.strictEqual(add(-1, 1), 0, "-1 + 1 should equal 0");
-assert.strictEqual(add(0, 0), 0, "0 + 0 should equal 0");
+assert.strictEqual(subtract(5, 2), 3, "5 - 2 should equal 3");
+assert.strictEqual(multiply(3, 4), 12, "3 * 4 should equal 12");
 
 console.log("All tests passed!");
